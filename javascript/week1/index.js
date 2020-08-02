68c1254d3f8316d40a19b257f1698fe93d806b06
tstancell <tracy.stancell@bryanuniversity.edu>
2020-08-02 15:40:44 -0400

    javascript week1

---
+++ javascript/week1/index.js
@@ -0,0 +1,17 @@
+var shopper = {
+    first_name: "Megan",
+    last_name: "brown",
+    age: 25,
+    married: true,
+    grocery_cart: ["fruit", "fish", "meat", "vegetables"],
+    cart_items: function() {
+        return this.fruit, this.fish, this.meat, this.vegetables; 
+        
+        
+    }
+
+};
+
+console.log("fruit, fish, meat, vegetables,");
+
+
